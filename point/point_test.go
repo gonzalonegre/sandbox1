83f13b994d1ff11091e467b34c6f12de59e6f91d
gonzalonegre <gonzalo.negre@gmail.com>
2018-11-26 15:44:43 -0300

    update

--- point/point_test.go
+++ point/point_test.go
@@ -7,7 +7,7 @@ import (
 );
 
 func TestDistance(t *testing.T) {
-        P1 := point.Point{X:1,Y:1}
+        P1 := point.Point{1,1}
         P2 := point.Point{4,5}
 	if (P1.Distance(P2) != 5) {
 		t.Fatalf("Expected %v, got: %v", math.Sqrt(25), P1.Distance(P2))
